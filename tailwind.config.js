ae15090917b80afcde312c1471695ce9416cbdfd
« Hassina <hassinaayachi@hotmail.com>
2023-11-20 15:58:39 +0100

    :sparkles: add header, hero and cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontSize: {
+      'paragraph': '16px',
+    },
+    extend: {
+      colors: {
+        'primary': '#2E69FF',
+      },
+      fontFamily: {
+        'font-primary': 'Exo, sans-serif',
+      },
+      gridTemplateColumns: {
+        'hero-input': '1fr, max-content',
+      }
+    },
   },
   plugins: [],
 }
